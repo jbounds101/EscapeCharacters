132c4bd13acadcf359c1607bf2a26b805a5d3bb9
jbounds101 <jackson.bounds@gmail.com>
2022-03-07 18:18:31 -0500

    Added script

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <string>
+#include <cstdio>
+
+std::string escapeCharacters(const std::string&);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string toFix = "\"He\?llo b\'etween quotes\"";
+    toFix = escapeCharacters(toFix);
+    std::cout << toFix << "\n";
+
     return 0;
 }
+
+std::string escapeCharacters(const std::string& text) {
+    std::string fixedText;
+    char currentChar;
+    for (int i = 0; i < text.length(); ++i) {
+        currentChar = text[i];
+        if (currentChar == '\\') {
+            char nextChar = text[i + 1];
+            fixedText.push_back(nextChar);
+            i += 2;
+        } else {
+            fixedText.push_back(currentChar);
+        }
+    }
+    return fixedText;
+}
+
